420239f09e31ca18a181ae80b268ef86242afe1b
mohit chandane <mohit.chandane@global.ntt>
2020-05-04 22:23:02 +0530

    changes 2

---
+++ src/app/UserDetails.ts
@@ -0,0 +1,9 @@
+export interface UserDetails {
+    firstName?: string;
+    lastName?: string;
+    mobileNumber?: string;
+    address?: string;
+    emailID?: string;
+    username?: string;
+    password?: string;
+}
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,10 @@
 import { RegisterUserService } from './../register-user.service';
+// tslint:disable-next-line: ordered-imports
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+// tslint:disable-next-line: ordered-imports
 import { FormGroup, FormControl } from '@angular/forms';
+import { UserDetails } from '../UserDetails';
 
 @Component({
   selector: 'app-register',
@@ -9,36 +12,34 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  hideRegisterPage: boolean;
+  public hideRegisterPage: boolean;
   public name: string;
   public detailsForm: FormGroup;
-  DBdata;
+  public userData: UserDetails;
+  public DBdata;
   constructor(private router: Router, private registerUserSer: RegisterUserService) { }
 
-  ngOnInit() {
+  public ngOnInit() {
     this.initForm();
-    this.registerUserSer.GetUser().subscribe((data) =>  {
-      console.log('oninit data is ', data);
-      this.DBdata = data;
-    });
+    this.userData = {};
   }
-  onClickCancel() {
+  public onClickCancel() {
     this.router.navigateByUrl('');
     this.hideRegisterPage = true;
   }
-  initForm() {
+  public initForm() {
     this.detailsForm = new FormGroup({
+      address: new FormControl(''),
+      emailID: new FormControl(''),
       firstName: new FormControl(''),
       lastName: new FormControl(''),
       mobileNumber: new FormControl(''),
-      address: new FormControl(''),
-      emailID: new FormControl(''),
-      username: new FormControl(''),
       password: new FormControl(''),
-   });
+      username: new FormControl(''),
+    });
   }
 
-  onClickSubmit() {
+  public onClickSubmit() {
     this.name = this.detailsForm.controls.firstName.value;
     // this.registerUserSer.getAllUsers().subscribe(data => {
     //   console.log('data respose', data);
@@ -55,5 +56,15 @@ export class RegisterComponent implements OnInit {
     // this.registerUserSer.postUser({name: 'user1'}).subscribe(data => {
     //   console.log('post data ', data);
     // });
+    this.name = this.detailsForm.controls.firstName.value;
+    console.log('usererrrrrrr', this.name);
+    this.userData.firstName = this.detailsForm.controls.firstName.value;
+    this.userData.lastName = this.detailsForm.controls.lastName.value;
+    this.userData.mobileNumber = this.detailsForm.controls.mobileNumber.value;
+    this.userData.address = this.detailsForm.controls.address.value;
+    this.userData.emailID = this.detailsForm.controls.emailID.value;
+    this.userData.username = this.detailsForm.controls.username.value;
+    this.userData.password = this.detailsForm.controls.password.value;
+    console.log('user data', this.userData);
   }
 }
